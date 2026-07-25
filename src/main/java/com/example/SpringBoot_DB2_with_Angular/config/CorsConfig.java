35c9961f3f37ed809d6e30be3a02f629ed4b25ec
Sandra <sandrasherin321@gmail.com>
2026-07-25 09:34:09 +0530

    commit all changes

---
+++ src/main/java/com/example/SpringBoot_DB2_with_Angular/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.example.SpringBoot_DB2_with_Angular.config;
+
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins("https://fluffy-ganache-20a6fd.netlify.app")
+                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                .allowedHeaders("*");
+    }
+}
